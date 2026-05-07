387532fed92c00c53377114d0cbe024836fb555e
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-07 22:32:38 +0700

    Adding README.md

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	server := gin.Default()
+
+	server.GET("/events", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	err := server.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 
 }
